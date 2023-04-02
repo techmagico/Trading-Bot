426dd50ba1f41ad580629af9b487c09ac26263e7
Mohammed Khaisar <mdkhaisarswork118@gmail.com>
2023-04-02 15:36:38 +0530

    Initial Commit

---
+++ ReactDashboardApi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReactdashboardapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ReactDashboardApi'
---
+++ ZerodhaApi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ZerodhaapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ZerodhaApi'
